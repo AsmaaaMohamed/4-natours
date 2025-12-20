39ac8ae86a3255aa4c3c6478c9432671464fa6ac
Asmaa Mohamed <eng_asmaaa20102000@yahoo.com>
2025-12-20 13:00:06 +0200

    comit eslint config

--- app.js
+++ app.js
@@ -1,50 +1,94 @@
 const express = require('express');
 const fs = require('fs');
+
 const app = express();
 app.use(express.json());
 
 const tours = JSON.parse(
-    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`),
 );
-app.get('/api/v1/tours', (req, res) => {
-    res.status(200).json({
-        status:"success",
-        results:tours.length,
-        data:{
-            tours
-        }
+const getAllTours = (req, res) => {
+  res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+        tours,
+        },
+  });
+};
+const getTour = (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
     });
-});
-app.get('/api/v1/tours/:id', (req, res) => {
-    const id = req.params.id * 1;
-    const tour = tours.find(tour => tour.id === id);
-    if(!tour){
-        return res.status(404).json({
-            status:"fail",
-            message:"Invalid ID"
-        })
-    }
-    res.status(200).json({
-        status:"success",
-        data:{
-            tour
-        }
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+};
+const createTour = (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  // eslint-disable-next-line prefer-object-spread
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours, null, 2),
+    () => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    },
+  );
+};
+const deleteTour = (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
     });
-});
-app.post('/api/v1/tours', (req, res) => {
-    const newId =tours[tours.length-1].id+1;
-    const newTour = Object.assign({id:newId},req.body);
-    tours.push(newTour);
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours, null, 2), err=>{
-        res.status(201).json({
-            status:"success",
-            data:{
-                tour:newTour
-            }
-        })
-    });    
-});
-
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+};
+const updateTour = (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+};
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+  // eslint-disable-next-line no-console
+  console.log('Server is running on port 3000');
 });
