044325d2778833c265903278074110bc43648547
Asmaa Mohamed <33634459+AsmaaaMohamed@users.noreply.github.com>
2026-01-03 16:52:57 +0200

    comit

--- models/tourModel.js
+++ models/tourModel.js
@@ -75,8 +75,40 @@ const tourSchema = new mongoose.Schema({
       type: Boolean,
       default: false
     }
+},
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+  });
+tourSchema.virtual('durationWeeks').get(function() {
+  return this.duration / 7;
+});
+// DOCUMENT MIDDLEWARE: runs before .save() and .create()
+tourSchema.pre('save', function(next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+// QUERY MIDDLEWARE
+// tourSchema.pre('find', function(next) {
+tourSchema.pre(/^find/, function(next) {
+  this.find({ secretTour: { $ne: true } });
+
+  this.start = Date.now();
+  next();
 });
 
+tourSchema.post(/^find/, function(docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds!`);
+  next();
+});
+
+// AGGREGATION MIDDLEWARE
+tourSchema.pre('aggregate', function(next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+
+  console.log(this.pipeline());
+  next();
+});
 const Tour = mongoose.model('Tour', tourSchema);
 
 const testTour = new Tour({
